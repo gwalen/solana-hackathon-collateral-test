4445d9afc208852b3bdeb4361800efb2633fe345
Grzegorz Walen <gwalen@git>
2021-04-10 19:56:05 +0200

    Serialize HashMap with client collateral -WIP1

--- src/program-rust/src/state.rs
+++ src/program-rust/src/state.rs
@@ -8,6 +8,9 @@ use arrayref::{array_mut_ref, array_refs, mut_array_refs};
 
 use std::collections::HashMap;
 
+//TODO: work with fixed size od hash map like here : https://github.com/Arrowana/solanalotto/blob/main/program/src/state.rs
+// look in solona-program-library using borsh : https://github.com/solana-labs/solana-program-library/blob/58807c5f8b9114545080ff4d1f98899246308ffc/record/program/src/state.rs
+
 pub struct CollateralData {
     pub is_initialized: bool,
     pub initializer_pubkey: Pubkey,
